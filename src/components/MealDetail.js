3db7f86a25f5be66eafd2ccd3777c99ee3a9341d
zakysirclo <zaky.arifudin@sirclo.com>
2021-10-01 16:00:18 +0700

    feat([111] Meal Detail): Ingredients table etc

--- src/components/MealDetail.js
+++ src/components/MealDetail.js
@@ -6,12 +6,35 @@ import {
   CardMedia,
   CardContent,
   Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
 } from "@mui/material";
 import Head from "next/head";
 import ReactPlayer from "react-player";
 
 export default function MealDetail({ meal }) {
   console.log(meal);
+
+  let items = [];
+
+  // handle ingredient => karena key ingredientnya tidak teratur isinya dari API
+  for (let i = 1; i <= 20; i++) {
+    if (
+      meal[`strIngredient${i}`] !== null &&
+      meal[`strIngredient${i}`] !== ""
+    ) {
+      items.push({
+        ingredient: meal[`strIngredient${i}`],
+        measure: meal[`strMeasure${i}`],
+      });
+    }
+  }
+
   return (
     <>
       <Head>
@@ -33,27 +56,50 @@ export default function MealDetail({ meal }) {
               alt={meal.strMeal}
             />
             <CardContent>
-              <Typography variant="h6" component="div" align="center">
-                {meal.strMeal}
+              <Typography variant="body1" component="div" align="center">
+                Tags : {meal.strTags}
+              </Typography>
+              <Typography variant="body2" component="div" align="center">
+                Category : {meal.strCategory}
               </Typography>
             </CardContent>
-            {/* <CardMedia
-              component="iframe"
-              height="300"
-              src={meal.strYoutube}
-              title={meal.strMeal}
-            /> */}
-            {/* <CardMedia
-              component="iframe"
-              height="300"
-              src={meal.strYoutube}
-              alt={meal.strMeal}
-            /> */}
           </Box>
         </Card>
+
+        <Container sx={{ my: "3rem" }}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Ingredients
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell width={50}>No.</TableCell>
+                  <TableCell align="left">Ingredient</TableCell>
+                  <TableCell align="left">Measure</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map((item, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row" width={50}>
+                      {index + 1}.
+                    </TableCell>
+                    <TableCell align="left">{item.ingredient}</TableCell>
+                    <TableCell align="left">{item.measure}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+
         <div style={{ margin: "3rem 0" }}>
           <ReactPlayer
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            url={meal.strYoutube}
             width="100%"
             height={400}
             playing={false}
